eee304415963e89099d71d03c34247d76e7398ba
Effi Amar <seffia@campus.technion.ac.il>
2019-12-27 09:46:56 +0200

    Started to implement RPC.

---
+++ src/main/java/protos/VoteServiceImpl.java
@@ -0,0 +1,21 @@
+package protos;
+
+import io.grpc.stub.StreamObserver;
+
+public class VoteServiceImpl extends VoteServiceGrpc.VoteServiceImplBase {
+
+    @Override
+    public void addVoteInternal(VoteRequest request, StreamObserver<VoteResponse> responseObserver) {
+
+        String greeting = new StringBuilder()
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
+
+}
